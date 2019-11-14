3e8cf82a47aea9f37d998b2f5653257ab7761c5e
Alexis Salcedo Salgado <geiner.salcedo.ppc0111@gmail.com>
2019-11-13 23:01:32 -0500

    ajuste apikey

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,14 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-
-
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [HeaderComponent, FooterComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MatToolbarModule,
+    MatIconModule
   ],
-  exports: [HeaderComponent, FooterComponent],
+  exports: [
+    HeaderComponent,
+    FooterComponent,
+    MatToolbarModule,
+    MatIconModule
+  ],
 })
 export class CoreModule { }
